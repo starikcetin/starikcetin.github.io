3007fb2141ab3f7e9c587f8fdf31062575223f55
starikcetin <cetinsamedtarik@gmail.com>
2023-08-09 02:04:10 +0300

    eliminate liquid whitespace in output

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -61,10 +61,10 @@ const setup = () => {
 window.addEventListener("DOMContentLoaded", setup);
 window.addEventListener("load", setup);
 
-{% if jekyll.environment == "production" and site.google_analytics %}
+{%- if jekyll.environment == "production" and site.google_analytics -%}
   window['ga-disable-{{ site.google_analytics }}'] = window.doNotTrack === "1" || navigator.doNotTrack === "1" || navigator.doNotTrack === "yes" || navigator.msDoNotTrack === "1";
   window.dataLayer = window.dataLayer || [];
   function gtag() { dataLayer.push(arguments); }
   gtag('js', new Date());
   gtag('config', '{{ site.google_analytics }}');
-{% endif %}
+{%- endif -%}
